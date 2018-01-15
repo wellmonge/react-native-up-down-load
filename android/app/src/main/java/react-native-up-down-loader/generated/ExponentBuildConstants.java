c0df2ca7866cee96533e7e898f1fb2eadcfb1759
MONGE-PC\wellinton <mongewellinton@gmail.com>
2018-01-15 18:32:43 -0300

    commit code structure for rnud-loader

---
+++ android/app/src/main/java/react-native-up-down-loader/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package react-native-up-down-loader.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
