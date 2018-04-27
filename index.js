445153788986622f4f312315d0755fc4eafddea2
wellmonge <wellitonmonge@outlook.com>
2018-04-27 13:20:11 -0300

    ...

--- index.js
+++ index.js
@@ -3,33 +3,31 @@ import { Platform } from 'react-native';
 var UpdownloadManager = require('./component').component;
 
 const postFormData = (config, successCallback, cancelCallback) => {
-    if (Platform.OS == 'android') {
+    if (Platform.OS === 'android') {
         UpdownloadManager.postFormData(config, successCallback, cancelCallback);
     }
 }
 
 const openSelectDialog = (config, successCallback, cancelCallback) => {
-    if (Platform.OS == 'android') {
+    if (Platform.OS === 'android') {
         UpdownloadManager.openSelectDialog(config, successCallback, cancelCallback);
     }
 }
 
 const showFile = (URLtoDownload) => {
-    if (Platform.OS == 'android') {
+    if (Platform.OS === 'android') {
         UpdownloadManager.showFile(URLtoDownload);
     }
 }
 
 const downloadFile = (URLtoDownload) => {
-    if (Platform.OS == 'android') {
+    if (Platform.OS === 'android') {
         UpdownloadManager.downloadFile(URLtoDownload);
     }
 }
 
 const teste = (message) => {
-    if (Platform.OS == 'android') {
-        UpdownloadManager.teste(message);  
-    }  
+    console.log("teste")
 }
 
 export {
